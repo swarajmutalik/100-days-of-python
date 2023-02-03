56739871f95d373e97fd25fbfdf1ccd101cc0f68
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-02-04 00:26:42 +0530

    Create main.py

---
+++ Comments,Escape Sequence and print starement/main.py
@@ -0,0 +1,14 @@
+'''
+Hey Swaraj, Please don't remove this line
+Author:Swaraj
+COurse:100DaysOfCode
+'''
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy\girl")
+
+# print("Hello World")
+# This is a 'Single-Line-Comment'
+# print("This is a print statement.")
+# print("Hello World!!!")
+
+print("Hey",6,7,sep="~",end="009\n")
+print("Swaraj")
